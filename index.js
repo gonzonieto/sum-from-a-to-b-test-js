6c702bf64c7bcd13ce5ecf0de3ae4e7bf500e003
‘Gonzo <gonzonieto@gmail.com>
2022-03-21 04:09:20 +0000

    solved!

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = (fromN, toN) => {
+  return fromN === toN ? fromN : fromN + sum(fromN + 1, toN);
+};
 
 module.exports = sum;
